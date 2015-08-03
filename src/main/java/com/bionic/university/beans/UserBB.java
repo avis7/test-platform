82081cc4dfde6787be37d417c5977d79e2b85c58
Sergiy Seleznyov <SergiySeleznyov@gmail.com>
2015-08-03 18:14:01 +0300

    Add logout(user backing bean with logout) and modify temlplate.xhtml(need fixes)

---
+++ src/main/java/com/bionic/university/beans/UserBB.java
@@ -0,0 +1,20 @@
+package com.bionic.university.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ * Created by Sergiy Seleznyov on 03.08.2015.
+ */
+
+//user backing bean
+@ManagedBean
+@SessionScoped
+public class UserBB {
+
+    public String logout(){
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        return "index.html?faces-redirect=true";
+    }
+}
