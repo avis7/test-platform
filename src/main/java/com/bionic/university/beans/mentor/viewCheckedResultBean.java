ad05a434bfc71a38b36c134953897af9c2079cab
alexbalitsky <alexbalitskyformore@gmail.com>
2015-08-09 16:28:57 -0700

    Update viewCheckedResultBean.java

--- src/main/java/com/bionic/university/beans/mentor/viewCheckedResultBean.java
+++ src/main/java/com/bionic/university/beans/mentor/viewCheckedResultBean.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 @ViewScoped
 @ManagedBean
-public class viewCheckedResultBean {
+public class ViewCheckedResultBean {
     private List<UserAnswer> userAnswers;
     private List<Question> questions;
 
